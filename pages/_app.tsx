32bde9b7972e50f6921f604248df59c4545297a0
s7tya <53410646+s7tya@users.noreply.github.com>
2021-12-25 01:02:11 +0900

    update: ChakraUIに変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import "ress";
-import "../styles/global.scss";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
