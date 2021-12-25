7be7e138bdf32f43c09c3599aa35028e75def8a3
s7tya <53410646+s7tya@users.noreply.github.com>
2021-12-25 10:35:14 +0900

    [WIP] add: indexページを実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  Heading,
+  Img,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <Container maxW="container.lg">
+      <VStack spacing="40px" py="100px">
+        <VStack spacing="28px">
+          <VStack spacing="8px">
+            <Heading fontSize="28px">簡単操作で漫画を一括投稿</Heading>
+            <Text textAlign="center">
+              一枚一枚画像を選んで 順番を間違えてやり直し...
+              <br />
+              Samari(サマリ)なら一括で選択して後から並べ替えるのも簡単です
+            </Text>
+          </VStack>
+          <ButtonGroup>
+            <Button as="a" bg="brand.500" color="black">
+              詳細を見る
+            </Button>
+          </ButtonGroup>
+        </VStack>
+        <Img src="/thumbnail.png" rounded="lg" />
+      </VStack>
+    </Container>
+  );
 };
 
 export default Home;
