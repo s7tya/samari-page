c506ac078ca2f175803f62a862cb0f133d28dd3d
s7tya <53410646+s7tya@users.noreply.github.com>
2021-12-27 19:57:51 +0900

    add: トップページを作る

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {
   Container,
   Heading,
   Img,
+  Stack,
   Text,
   VStack,
 } from "@chakra-ui/react";
@@ -11,26 +12,31 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Container maxW="container.lg">
-      <VStack spacing="40px" py="100px">
-        <VStack spacing="28px">
-          <VStack spacing="8px">
+    <>
+      <Stack spacing="40px" pt="100px" px="20px" alignItems="center">
+        <Stack spacing="28px" alignItems="center">
+          <Stack spacing="8px" alignItems="center">
             <Heading fontSize="28px">簡単操作で漫画を一括投稿</Heading>
             <Text textAlign="center">
               一枚一枚画像を選んで 順番を間違えてやり直し...
               <br />
               Samari(サマリ)なら一括で選択して後から並べ替えるのも簡単です
             </Text>
-          </VStack>
+          </Stack>
           <ButtonGroup>
-            <Button as="a" bg="brand.500" color="black">
+            <Button
+              as="a"
+              bg="brand.500"
+              color="black"
+              href="https://zenn.dev/s7/scraps/1f58a6cf883b6e"
+            >
               詳細を見る
             </Button>
           </ButtonGroup>
-        </VStack>
+        </Stack>
         <Img src="/thumbnail.png" rounded="lg" />
-      </VStack>
-    </Container>
+      </Stack>
+    </>
   );
 };
 
