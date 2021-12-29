6dfa1c398dae04a1db8e4b9e267adc24b30d710f
s7tya <53410646+s7tya@users.noreply.github.com>
2021-12-29 17:52:53 +0900

    update: TweetCardを画像の枚数に応じて表示を変更するように

--- src/components/TweetCard.tsx
+++ src/components/TweetCard.tsx
@@ -44,19 +44,104 @@ export const TweetCard: React.FC<TweetCardProps> = ({
         ></Box>
         <Stack pb="16px">
           <Text>{body}</Text>
-          <Grid
-            templateColumns="1fr 1fr"
-            templateRows="1fr 1fr"
-            gap="2px"
-            rounded="md"
-            overflow="hidden"
-          >
-            {images.map((image, index) => (
-              <GridItem maxH="200px" key={index}>
-                <Img h="full" w="full" objectFit="cover" src={image.preview} />
-              </GridItem>
-            ))}
-          </Grid>
+          {images.length === 1 && (
+            <Grid
+              templateColumns="1fr"
+              templateRows="1fr"
+              gap="2px"
+              rounded="md"
+              overflow="hidden"
+            >
+              {images.map((image, index) => (
+                <GridItem maxH="240px" key={index}>
+                  <Img
+                    h="full"
+                    w="full"
+                    objectFit="cover"
+                    src={image.preview}
+                  />
+                </GridItem>
+              ))}
+            </Grid>
+          )}
+          {images.length === 2 && (
+            <Grid
+              templateColumns="1fr 1fr"
+              templateRows="1fr"
+              gap="2px"
+              rounded="md"
+              overflow="hidden"
+            >
+              {images.map((image, index) => (
+                <GridItem h="full" maxH="200px" key={index}>
+                  <Img
+                    h="full"
+                    w="full"
+                    objectFit="cover"
+                    src={image.preview}
+                  />
+                </GridItem>
+              ))}
+            </Grid>
+          )}
+          {images.length === 3 && (
+            <Grid
+              templateColumns="1fr 1fr"
+              templateRows="1fr 1fr"
+              gap="2px"
+              rounded="md"
+              overflow="hidden"
+            >
+              {images.map((image, index) => (
+                <>
+                  {index === 0 ? (
+                    <GridItem
+                      maxH="400px"
+                      key={index}
+                      gridColumn="1"
+                      gridRow="1 / 3"
+                    >
+                      <Img
+                        h="full"
+                        w="full"
+                        objectFit="cover"
+                        src={image.preview}
+                      />
+                    </GridItem>
+                  ) : (
+                    <GridItem maxH="200px" key={index}>
+                      <Img
+                        h="full"
+                        w="full"
+                        objectFit="cover"
+                        src={image.preview}
+                      />
+                    </GridItem>
+                  )}
+                </>
+              ))}
+            </Grid>
+          )}
+          {images.length === 4 && (
+            <Grid
+              templateColumns="1fr 1fr"
+              templateRows="1fr 1fr"
+              gap="2px"
+              rounded="md"
+              overflow="hidden"
+            >
+              {images.map((image, index) => (
+                <GridItem maxH="200px" key={index}>
+                  <Img
+                    h="full"
+                    w="full"
+                    objectFit="cover"
+                    src={image.preview}
+                  />
+                </GridItem>
+              ))}
+            </Grid>
+          )}
         </Stack>
       </Grid>
     </Stack>
